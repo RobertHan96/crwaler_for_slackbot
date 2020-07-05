0fe03656f2fa3475ccbf9e4f5a7e2e4dc7659da8
RobertHan96 <yshan4329@gmail.com>
2020-07-05 15:06:49 +0900

    유저 입력 메세지에 따른 챗봇 동작 처리

--- venv/Coin.py
+++ venv/Coin.py
@@ -1,24 +1,42 @@
 from selenium import webdriver
 from CoinInfo import CoinInfo
-
 import time
-driver_path = '/Users/mac/0_Dev/PythonProjects/get_coin/chromedriver'
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(driver_path)
-driver.get('http://luka7.net/')
-time.sleep(3)
 
-search_kewords = ['BTC', 'XRP']
-coin_infos = []
+class Coin:
+    def getCoinInfo(self):
+        driver_path = '/Users/mac/0_Dev/PythonProjects/get_coin/chromedriver'
+        driver = webdriver.Chrome(driver_path)
+        url = 'http://luka7.net/'
+        driver.get(url)
+        # driver.implicitly_wait(10)
+        WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#coinList>div.BTC>div.minus")))
 
-for coin in search_kewords :
-    coin_row = driver.find_element_by_css_selector('.{}'.format(coin))
-    coin_name = coin_row.find_element_by_css_selector('.coin').text
-    coin_usd = coin_row.find_element_by_css_selector('.usd').text
-    coin_krw = coin_row.find_element_by_css_selector('.krw').text
-    coin_price_diff = coin_row.find_element_by_css_selector('.minus').text
+        search_kewords = ['BTC', 'XRP']
+        coin_infos = []
+        for coin in search_kewords :
+            coin_row = driver.find_element_by_css_selector('.{}'.format(coin))
+            coin_name = coin_row.find_element_by_css_selector('.coin').text
+            coin_usd = coin_row.find_element_by_css_selector('.usd').text
+            coin_krw = coin_row.find_element_by_css_selector('.krw').text
+            coin_price_diff = coin_row.find_element_by_css_selector('.minus').text
+            coin_info = CoinInfo(coin_name, coin_usd, coin_krw, coin_price_diff)
+            coin_infos.append(coin_info)
+        result_text = '''
+        오늘의 코인 가격 정보
+        {} :
+        원화 가격 : {}
+        달러 가격 : {}
+        전일 대비 증/감 : {}
 
-    coin_info = CoinInfo(coin_name, coin_usd, coin_krw, coin_price_diff)
-    coin_infos.append(coin_info)
-
-driver.close()
+        {} :
+        원화 가격 : {}
+        달러 가격 : {}
+        전일 대비 증/감 : {}
+        '''.format(coin_infos[0].name, coin_infos[0].price_krw, coin_infos[0].price_usd, coin_infos[0].price_diff,
+                   coin_infos[1].name, coin_infos[1].price_krw, coin_infos[1].price_usd, coin_infos[1].price_diff)
+        return result_text
+        driver.close()
--- venv/wanted.py
+++ venv/wanted.py
@@ -2,33 +2,37 @@ from WantedInfo import WantedInfo
 from selenium import webdriver
 import time
 
-driver_path = '/Users/mac/0_Dev/PythonProjects/get_coin/chromedriver'
-url = 'https://www.wanted.co.kr/wdlist/518/678?country=kr&job_sort=job.latest_order&years=0&locations=all'
-driver = webdriver.Chrome(driver_path)
-driver.get(url)
-wanted_jobs = []
-url_list = []
-time.sleep(3)
+class Wanted :
+    def getWantedInfo(self):
+        driver_path = '/Users/mac/0_Dev/PythonProjects/get_coin/chromedriver'
+        url = 'https://www.wanted.co.kr/wdlist/518/678?country=kr&job_sort=job.latest_order&years=0&locations=all'
+        driver = webdriver.Chrome(driver_path)
+        driver.get(url)
+        wanted_jobs = []
+        url_list = []
+        time.sleep(10)
 
-datas = driver.find_element_by_css_selector('ul.clearfix')
-companies = datas.find_elements_by_class_name('body')
-urls = datas.find_elements_by_tag_name('a')
+        datas = driver.find_element_by_css_selector('ul.clearfix')
+        companies = datas.find_elements_by_class_name('body')
+        urls = datas.find_elements_by_tag_name('a')
 
-for url in urls :
-    detail_link = url.get_property('href')
-    url_list.append(detail_link)
-i = 0
-for company in companies :
-    position_name = company.find_element_by_class_name('job-card-position').text
-    name = company.find_element_by_class_name('job-card-company-name').text
-    location = company.find_element_by_class_name('job-card-company-location').text
-    reward = company.find_element_by_class_name('reward').text
-    url = url_list[i]
-    i += 1
-    wanted_jobs.append(WantedInfo(name, location,position_name, reward, url))
+        for url in urls :
+            detail_link = url.get_property('href')
+            url_list.append(detail_link)
+        i = 0
+        for company in companies :
+            position_name = company.find_element_by_class_name('job-card-position').text
+            name = company.find_element_by_class_name('job-card-company-name').text
+            location = company.find_element_by_class_name('job-card-company-location').text
+            reward = company.find_element_by_class_name('reward').text
+            url = url_list[i]
+            i += 1
+            wanted_jobs.append(WantedInfo(name, location,position_name, reward, url))
 
-print(len(wanted_jobs))
-for job in wanted_jobs :
-    print(job.name)
+        response_text = '원티드에서 찾은 {}개의 개발자 채용정보'.format(len(wanted_jobs))
+        for job in wanted_jobs :
+            response_text = response_text + '\n{} / {} / {}\n{}\n{}'.format(
+                job.name, job.position_name, job.location, job.reward, job.url)
 
-driver.close()
+        driver.close()
+        return response_text
